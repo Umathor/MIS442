63da4e78c7cff6608d1e769c064fe78e22717854
Dan Seger <dan.seger@oit.edu>
2025-11-02 18:48:39 -0800

    product classes created. Productprod finished, productDB create and delete finished.

---
+++ Lab3/framework_lab3_2023_Starterfiles/MMABooksFramework2022/MMABooksDB/ProductDB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+using MMABooksTools;
+using MMABooksProps;
+
+using System.Data;
+
+// *** I use an "alias" for the ado.net classes throughout my code
+// When I switch to an oracle database, I ONLY have to change the actual classes here
+using DBBase = MMABooksTools.BaseSQLDB;
+using DBConnection = MySql.Data.MySqlClient.MySqlConnection;
+using DBCommand = MySql.Data.MySqlClient.MySqlCommand;
+using DBParameter = MySql.Data.MySqlClient.MySqlParameter;
+using DBDataReader = MySql.Data.MySqlClient.MySqlDataReader;
+using DBDataAdapter = MySql.Data.MySqlClient.MySqlDataAdapter;
+using DBDbType = MySql.Data.MySqlClient.MySqlDbType;
+using System.Collections.Generic;
+
+namespace MMABooksDB
+{
+    public class ProductDB : DBBase, IReadDB, IWriteDB
+    {
+        public ProductDB() : base() { }
+        public ProductDB(DBConnection cn) : base(cn) { }
+
+        public IBaseProps Create(IBaseProps p)
+        {
+            int rowsAffected = 0;
+            ProductProps props = (ProductProps)p;
+
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_ProductCreate";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("prodId", 0); // output parameter
+            command.Parameters["prodId"].Direction = ParameterDirection.Output;
+            command.Parameters.AddWithValue("productCode_p", props.ProductCode);
+            command.Parameters.AddWithValue("description_p", props.Description);
+            command.Parameters.AddWithValue("unitprice_p", props.UnitPrice);
+            command.Parameters.AddWithValue("onhandquantity_p", props.OnHandQuantity);
+            try
+            {
+                rowsAffected = RunNonQueryProcedure(command);
+                if (rowsAffected == 1)
+                {
+                    props.ProductID = Convert.ToInt32(command.Parameters["productId"].Value);
+                    props.ConcurrencyID = 1;
+                    return props;
+                }
+                else
+                    throw new Exception("Unable to insert record. " + props.GetState());
+            }
+            catch (Exception e)
+            {
+                // log this error
+                throw;
+            }
+            finally
+            {
+                if (mConnection.State == ConnectionState.Open)
+                    mConnection.Close();
+            }
+        }
+        public bool Delete(IBaseProps p)
+        {
+            int rowsAffected = 0;
+            ProductProps props = (ProductProps)p;
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_ProductDelete";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("prodId", DBDbType.Int32);)
+            command.Parameters.Add("conCurrId", DBDbType.Int32);
+            command.Parameters["prodId"].Value = props.ProductID;
+            command.Parameters["conCurrId"].Value = props.ConcurrencyID;
+
+            try
+            {
+                rowsAffected = RunNonQueryProcedure(command);
+                if (rowsAffected == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    string message = "Record cannot be deleted. It has been edited by another user.";
+                    throw new Exception(message);
+                }
+            }
+            catch (Exception e)
+            {
+                // log this error
+                throw;
+            }
+            finally
+            {
+                if (mConnection.State == ConnectionState.Open)
+                    mConnection.Close();
+            }
+
+        }
+    }
+}
