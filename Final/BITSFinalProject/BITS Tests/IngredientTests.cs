88ca6b66ff90ad9f64f0a634ab73fb21aa836764
Dan Seger <dan.seger@oit.edu>
2025-12-12 18:26:16 -0800

    Created tests for CRUD on recipe ingredient and ingredient classes.

--- Final/BITSFinalProject/BITS Tests/IngredientTests.cs
+++ Final/BITSFinalProject/BITS Tests/IngredientTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using BITSFinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,107 @@ using System.Threading.Tasks;
 
 namespace BITS_Tests
 {
-    internal class IngredientTests
+    [TestFixture]
+    public class IngredientTests
     {
+        BITSContext dbContext;
+        Ingredient? testIngredient;
+        List<Ingredient> ingredients;
+        [SetUp]
+        public void Setup()
+        {
+            dbContext = new BITSContext();
+            dbContext.Database.ExecuteSqlRaw("call usp_testingResetData");
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            dbContext.Database.ExecuteSqlRaw("call usp_testingResetData");
+        }
+        [TearDownAttribute]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+        [Test]
+        public void GetAllTest()
+        {
+            // Arrange
+            ingredients = dbContext.Ingredients.ToList();
+            // Act
+            int count = ingredients.Count;
+            // Assert
+            Assert.AreEqual(1149, count);
+        }
+        [Test]
+        public void GetByIdTest()
+        {
+            // Arrange
+            int idToFind = 1084;
+            // Act
+            testIngredient = dbContext.Ingredients.Find(idToFind);
+            // Assert. AI really struggled here, it thinks we are a bakery not a brewery.
+            Assert.IsNotNull(testIngredient);
+            Assert.AreEqual(1084, testIngredient.IngredientId);
+            Assert.AreEqual("Crystal", testIngredient.Name);
+            Assert.AreEqual(1, testIngredient.Version);
+            Assert.AreEqual(3, testIngredient.IngredientTypeId);
+            Assert.AreEqual(0, testIngredient.OnHandQuantity);
+            Assert.AreEqual(3, testIngredient.UnitTypeId);
+            Assert.AreEqual(0.00, testIngredient.UnitCost);
+            Assert.AreEqual(0, testIngredient.ReorderPoint);
+            Assert.AreEqual(133, testIngredient.Notes.Count());
+        }
+        [Test]
+        public void CreateTest()
+        {
+            // Arrange
+            Ingredient newIngredient = new Ingredient
+            {
+                Name = "Test Ingredient",
+                Version = 1,
+                IngredientTypeId = 1,
+                OnHandQuantity = 10,
+                UnitTypeId = 1,
+                UnitCost = 2.50m,
+                ReorderPoint = 5
+            };
+            // Act
+            dbContext.Ingredients.Add(newIngredient);
+            dbContext.SaveChanges();
+            // Assert
+            Assert.IsTrue(newIngredient.IngredientId > 0);
+            Ingredient? createdIngredient = dbContext.Ingredients.Find(newIngredient.IngredientId);
+            Assert.IsNotNull(createdIngredient);
+            Assert.AreEqual("Test Ingredient", createdIngredient.Name);
+        }
+        [Test]
+        public void UpdateTest()
+        {
+            // Arrange
+            int idToUpdate = 1084;
+            Ingredient? ingredientToUpdate = dbContext.Ingredients.Find(idToUpdate);
+            Assert.IsNotNull(ingredientToUpdate);
+            // Act
+            ingredientToUpdate.Name = "Updated Ingredient Name";
+            dbContext.SaveChanges();
+            // Assert
+            Ingredient? updatedIngredient = dbContext.Ingredients.Find(idToUpdate);
+            Assert.IsNotNull(updatedIngredient);
+            Assert.AreEqual("Updated Ingredient Name", updatedIngredient.Name);
+        }
+        [Test]
+        public void DeleteTest()
+        {
+            // Arrange
+            int idToDelete = 1084;
+            testIngredient = dbContext.Ingredients.Find(idToDelete);
+            Assert.IsNotNull(testIngredient);
+            // Act
+            dbContext.Ingredients.Remove(testIngredient);
+            // Assert
+            Assert.Throws<DbUpdateException>(() => dbContext.SaveChanges());
+        }
     }
 }
+
--- Final/BITSFinalProject/BITS Tests/RecipeIngredientTests.cs
+++ Final/BITSFinalProject/BITS Tests/RecipeIngredientTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BITSFinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,112 @@ using System.Threading.Tasks;
 
 namespace BITS_Tests
 {
-    internal class RecipeIngredientTests
+    [TestFixture]
+    public class RecipeIngredientTests
     {
+        BITSContext dbContext;
+        RecipeIngredient? testRecipeIngredient;
+        List<RecipeIngredient> recipeIngredients;
+
+        [SetUp]
+        public void Setup()
+        {
+            dbContext = new BITSContext();
+            dbContext.Database.ExecuteSqlRaw("call usp_testingResetData");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            dbContext.Database.ExecuteSqlRaw("call usp_testingResetData");
+        }
+        [TearDownAttribute]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+        //letting the AI take the wheel a bit for test generation. Using builting copilot autocomplete from here.
+        [Test]
+        public void GetAllTest()
+        {
+            // Arrange
+            recipeIngredients = dbContext.RecipeIngredients.ToList();
+            // Act
+            int count = recipeIngredients.Count;
+            // Assert
+            Assert.AreEqual(57, count);
+        }
+        [Test]
+        public void GetByIdTest()
+        {
+            // Arrange
+            int idToFind = 1;
+            // Act, AI wanted to pull the whole list then search it with where instead of using the existing find function
+            testRecipeIngredient = dbContext.RecipeIngredients.Find(idToFind);
+            // Assert
+            Assert.IsNotNull(testRecipeIngredient);
+            Assert.AreEqual(1, testRecipeIngredient.RecipeIngredientId);
+            Assert.AreEqual(1, testRecipeIngredient.RecipeId);
+            Assert.AreEqual(1084, testRecipeIngredient.IngredientId);
+            Assert.AreEqual(0.0283495, testRecipeIngredient.Quantity);
+            Assert.AreEqual(60, testRecipeIngredient.Time);
+            Assert.AreEqual(3, testRecipeIngredient.UseDuringId);
+        }
+        [Test]
+        public void CreateTest()
+        {
+            // Arrange
+            RecipeIngredient newRecipeIngredient = new RecipeIngredient
+            {
+                RecipeId = 2,
+                IngredientId = 1000,
+                Quantity = 1.5,
+                Time = 30,
+                UseDuringId = 1
+            };
+            // Act
+            dbContext.RecipeIngredients.Add(newRecipeIngredient);
+            dbContext.SaveChanges();
+            // Assert
+            Assert.IsTrue(newRecipeIngredient.RecipeIngredientId > 0);
+            RecipeIngredient? createdRecipeIngredient = dbContext.RecipeIngredients.Find(newRecipeIngredient.RecipeIngredientId);
+            Assert.IsNotNull(createdRecipeIngredient);
+            Assert.AreEqual(2, createdRecipeIngredient.RecipeId);
+            Assert.AreEqual(1000, createdRecipeIngredient.IngredientId);
+            Assert.AreEqual(1.5, createdRecipeIngredient.Quantity);
+            Assert.AreEqual(30, createdRecipeIngredient.Time);
+            Assert.AreEqual(1, createdRecipeIngredient.UseDuringId);
+        }
+        [Test]
+        public void UpdateTest()
+        {
+            // Arrange
+            int idToUpdate = 1;
+            testRecipeIngredient = dbContext.RecipeIngredients.Find(idToUpdate);
+            Assert.IsNotNull(testRecipeIngredient);
+            // Act
+            testRecipeIngredient.Quantity = 0.05;
+            testRecipeIngredient.Time = 45;
+            dbContext.SaveChanges();
+            // Assert
+            RecipeIngredient? updatedRecipeIngredient = dbContext.RecipeIngredients.Find(idToUpdate);
+            Assert.IsNotNull(updatedRecipeIngredient);
+            Assert.AreEqual(0.05, updatedRecipeIngredient.Quantity);
+            Assert.AreEqual(45, updatedRecipeIngredient.Time);
+        }
+        [Test]
+        public void DeleteTest()
+        {
+            // Arrange
+            int idToDelete = 1;
+            testRecipeIngredient = dbContext.RecipeIngredients.Find(idToDelete);
+            Assert.IsNotNull(testRecipeIngredient);
+            // Act
+            dbContext.RecipeIngredients.Remove(testRecipeIngredient);
+            dbContext.SaveChanges();
+            // Assert
+            RecipeIngredient? deletedRecipeIngredient = dbContext.RecipeIngredients.Find(idToDelete);
+            Assert.IsNull(deletedRecipeIngredient);
+        }
     }
 }
